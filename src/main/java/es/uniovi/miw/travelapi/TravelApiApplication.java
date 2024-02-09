b6a13f2869a8fa71b6053589352bcee9caef59ee
danielorviz <danielorviz@gmail.com>
2024-02-09 20:26:24 +0100

    fasdf

---
+++ src/main/java/es/uniovi/miw/travelapi/TravelApiApplication.java
@@ -0,0 +1,13 @@
+package es.uniovi.miw.travelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/uniovi/miw/travelapi/TravelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uniovi.miw.travelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
